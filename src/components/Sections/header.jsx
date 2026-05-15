c3d8221c472a3fda33a6a5f0d7a307062c9700ea
snielchevallier <snielchevallier@gmail.com>
2026-05-16 00:44:43 +0200

    finalisation page 404

--- src/components/Sections/header.jsx
+++ src/components/Sections/header.jsx
@@ -1,26 +1,30 @@
-  import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/slices/authSlice";
+import { useSelector } from "react-redux";
 
 import logo from '../../assets/logo.gif'
 import logoHeader from '../../assets/logo_header.svg'
 
 function Header() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   const dispatch = useDispatch();
   const handleLogout = (e) => {
-  e.preventDefault();
-  dispatch(logout());
-};
+    e.preventDefault();
+    dispatch(logout());
+  };
 
   return (
     <header className="bg-purple-light flex max-w-6xl mx-auto">
       <nav className="flex flex-row items-center justify-between w-full py-4 px-6">
         <NavLink to="/" className="flex flex-row"><img src={logo} alt="logo Sportsee" className="w-8 h-8" /> <img src={logoHeader} alt="Sportsee" className="h-8 ml-2" /></NavLink>
-        <ul className="flex flex-row items-center bg-white rounded-full py-4 px-14 gap-16">
-          <li><NavLink to="/dashboard">Dashboard</NavLink></li>
-          <li><NavLink to="/profile">Mon profil</NavLink></li>
-          <li className="border-l pl-16"><a href="#" onClick={handleLogout}>Se deconnecter</a></li>
-        </ul>
+        {isAuthenticated && (
+          <ul className="flex flex-row items-center bg-white rounded-full py-4 px-14 gap-16">
+            <li><NavLink to="/dashboard">Dashboard</NavLink></li>
+            <li><NavLink to="/profile">Mon profil</NavLink></li>
+            <li className="border-l pl-16"><a href="#" onClick={handleLogout}>Se deconnecter</a></li>
+          </ul>
+        )}
       </nav>
     </header>
   )
--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,12 +1,13 @@
-
+import { NavLink } from "react-router-dom"
 const Error = () => {
   return (
-    <div>
-      <h1 className="error-title">404</h1>
-      <h2>Oups 🙈 Cette page n'existe pas</h2>
-      <p>La page que vous cherchez semble introuvable.</p>
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1 className="text-2xl font-bold">erreur 404</h1>
+      <p className="text-">La page que vous cherchez semble introuvable.</p>
+      <NavLink to="/" className="bg-button text-button-text w-50 h-[51px] flex justify-center items-center hover:bg-button-hover cursor-pointer text-button-text py-2 px-4 rounded-xl duration-300">
+        Retour à l'accueil
+      </NavLink>
     </div>
   )
 }
-
 export default Error
